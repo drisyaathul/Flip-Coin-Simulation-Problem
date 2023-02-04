8a02599a35f8cd90502aa22ebc6dc89ed7c6d3bc
drisyaathul <drisyak809@gmail.com>
2023-02-04 21:42:24 +0800

    Added : Multiple times of flipping a coin

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-coin=$((RANDOM%2));
 
-if [ $(($coin)) -eq 0 ];
+Head=0;
+Tail=0;
 
-then
-	echo "Head";
 
-else
-	echo "Tail";
+for (( flip=1; flip<=30; flip++ ));
 
-fi
+do
+	result=$((RANDOM%2));
+	if [ $result -eq 0 ]
+	then 
+	Head=$(($Head+1));
+	else
+	Tail=$(($Tail+1));
+
+	fi
+
+done
+echo "No of times Head Won is equal to :" $Head
+echo "No of times Tail Won is equal to :" $Tail
